7ea63cc1a772153b432cd13ba5b14ede12dc91b2
surakshya <surakchya1234@gmail.com>
2025-04-11 11:51:23 +0545

    Started Portfolio

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+import About from "../components/about"
+export default function Aboutus(){
+    return(
+        <div>
+            <About />
+        </div>
+    )
+}
---
+++ src/app/components/about.tsx
@@ -0,0 +1,11 @@
+export default function About(){
+    return(
+        <section>
+      <h1>About Me</h1>
+      <p>
+        Hello! I'm a passionate  developer with a passion for creating interactive and responsive user experiences.
+        Welcome to my portfolio. Feel free to explore!
+      </p>
+    </section>
+    )
+}
